8941953eb10b467b71889c853403724e318f57d8
moosecoop <cooper@makhijani.us>
2017-04-30 11:57:15 -0700

    updates to kick and ban

--- commands/utility/ban.js
+++ commands/utility/ban.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 exports.run = (client, msg, [user]) => {
-    if (msg.author.hasPermission("BAN_MEMBERS")) { //admin: 308258523223949313 - mod: 308258939278065665
+    if (msg.author.highestRole.id == "308258523223949313" || "308258939278065665") { //admin: 308258523223949313 - mod: 308258939278065665
         let userToBan = msg.mentions.users.first();
         //we need to get a *GuildMember* object, mentions are only users. Then, we ban!
         msg.guild.member(userToBan).ban()
